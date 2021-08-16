1f22ca46b0bcbb2b5ce9f0df845f5630f5404b51
Natan Rodrigues de Oliveira <natanrodriguesdeoliveira@gmail.com>
2021-08-16 20:58:40 -0300

    [Milestone - Listar filmes e séries] Testes de integração dos endpoints finalizados.

--- src/test/java/br/com/example/demo/controller/TVSeriesControllerTest.java
+++ src/test/java/br/com/example/demo/controller/TVSeriesControllerTest.java
@@ -9,35 +9,31 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-    properties = {
-        "tmdb.api.key=1b7ac4704aa4d2e6bf887a3ec435f1c9"
-    }
-)
-class MoviesControllerTest {
-
-    private String BASE_URL_PATH = "/demo/v1";
+    properties = {"tmdb.api.key=1b7ac4704aa4d2e6bf887a3ec435f1c9"})
+class TVSeriesControllerTest {
     @LocalServerPort
     private Integer port;
+    private String BASE_URL_PATH = "/demo/v1";
 
     @Test
-    void shouldReturnMovieDetailsSchemaWithStatusCode200WhenCalledWithId497698() {
+    void shouldReturnTvSerieDetailsWith200StatusCodeWhenCalledWithId84958() {
         given()
             .basePath(BASE_URL_PATH)
             .port(port)
         .when()
-            .get("/movie/497698")
+            .get("/tv/84958")
         .then()
             .statusCode(200)
-            .body("id", is(497698));
+            .body("id", is(84958));
     }
 
     @Test
-    void shouldReturnMovieDetailsWithStatus404WhenCalledWithAnInvalidId49769800() {
+    void shouldReturnTvSeriesDetailsWith404StatusCodeWhenCalledWithId8495800() {
         given()
             .basePath(BASE_URL_PATH)
             .port(port)
         .when()
-            .get("/movie/49769800")
+            .get("/tv/8495800")
         .then()
             .statusCode(404)
             .body(containsString("title"),
@@ -47,29 +43,29 @@ class MoviesControllerTest {
     }
 
     @Test
-    void shouldReturnPopularMoviesWith200StatusCodeWhenCalledWithoutPageParam() {
+    void shouldReturnPopularTVShowsWith200StatusCodeWhenCalledWithoutPageParam() {
         given()
             .basePath(BASE_URL_PATH)
             .port(port)
         .when()
-            .get("/movies/popular")
+            .get("/tv/popular")
         .then()
             .statusCode(200);
     }
 
     @Test
-    void shouldReturnPopularMoviesWith200StatusCodeWhenCalledWithAnIntegerPageParam() {
+    void shouldReturnPopularTVShowsWith200StatusCodeWhenCalledWithAnIntegerPageParam() {
         given()
             .basePath(BASE_URL_PATH)
             .port(port)
         .when()
-            .get("/movies/popular?page=2")
+            .get("/tv/popular?page=2")
         .then()
             .statusCode(200);
     }
 
     @Test
-    void shouldReturnTopRatedMoviesWith200StatusCodeWhenCalledWithoutPageParam() {
+    void shouldReturnTopRatedTVShowsWith200StatusCodeWhenCalledWithoutPageParam() {
         given()
             .basePath(BASE_URL_PATH)
             .port(port)
@@ -80,7 +76,7 @@ class MoviesControllerTest {
     }
 
     @Test
-    void shouldReturnTopRatedMoviesWith200StatusCodeWhenCalledWithAnIntegerPageParam() {
+    void shouldReturnTopRatedTVShowsWith200StatusCodeWhenCalledWithAnIntegerPageParam() {
         given()
             .basePath(BASE_URL_PATH)
             .port(port)
@@ -89,4 +85,5 @@ class MoviesControllerTest {
         .then()
             .statusCode(200);
     }
+
 }
